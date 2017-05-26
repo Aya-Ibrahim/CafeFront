af42ccd04a642d92721d344171cd985d068d50ee
fe <fe@hadidy>
2017-05-26 13:44:15 +0200

    add gitignorefile

--- New Arkan/arkan/arkanCafeFrontEnd/web/resourses/pagesJs/waiter/home/homeController.js
+++ New Arkan/arkan/arkanCafeFrontEnd/web/resourses/pagesJs/waiter/home/homeController.js
@@ -5,11 +5,20 @@ angular.module('arkanCafe').controller('homeController', function ($scope, homeS
         {tableId: 2, tableName: 12, state: false},
         {tableId: 3, tableName: 14, state: true}
     ];
+    $scope.finishedOrders = [];
     $scope.goToOrder = function (table) {
 
 //        $state.go("/order/drinks/" + table.tableName + '/' + table.tableId);
         $state.go("order_table_drinks", {tableName: table.tableName, tableId: table.tableId}, {notify: true, reload: false});
     };
+    var getAllFinishedOrderOnSuccess = function (data, status, headers, config) {
+        $scope.finishedOrders = data;
+
+    }
+    var onError = function (data, status, headers, config) {
+        console.log(data);
+    };
+    homeService.getAllFinishedOrder(getAllFinishedOrderOnSuccess, onError);
 
 });
 
--- New Arkan/arkan/arkanCafeFrontEnd/web/resourses/pagesJs/waiter/home/homeService.js
+++ New Arkan/arkan/arkanCafeFrontEnd/web/resourses/pagesJs/waiter/home/homeService.js
@@ -1,4 +1,13 @@
-angular.module('arkanCafe').service('homeService', function () {
-    
+angular.module('arkanCafe').service('homeService', function (webServiceUrl, $http) {
+    var _this = this;
+    var orderWebServicePath = "order/";
+    this.getAllFinishedOrder = function (onSuccess, onError) {
+        var webServiceMethodPath = "getAllFinishedOrder";
+        $http({method: 'get', url: webServiceUrl + orderWebServicePath + webServiceMethodPath,
+            headers: {'Content-Type': 'application/json'}})
+                .success(onSuccess).error(onError)
+
+    }
+
 });
 
